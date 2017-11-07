3a16b06b9c1a8e7de3cae232b6fbc5104b7c8f4e
thewawar <thewawar@gmail.com>
2017-11-08 01:19:19 +0800

    The basic API

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+
+mod dom_string_renderer;
+mod partial_renderer;
+
+pub use partial_renderer::{
+    ReadSize,
+    DomServerRenderer,
+};
+pub use dom_string_renderer::{
+    render_to_string,
+    render_to_static_markup,
+};
+
+
 #[cfg(test)]
 mod tests {
     #[test]
